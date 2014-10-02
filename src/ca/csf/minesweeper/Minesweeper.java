3bb162bcd80fe7274332db9b9895328cdc6f8c33
Dominionized <dominique.begin.0@gmail.com>
2014-10-02 10:24:13 -0400

    Implemented constructor of Minesweeeper class with difficulty.

--- src/ca/csf/minesweeper/Minesweeper.java
+++ src/ca/csf/minesweeper/Minesweeper.java
@@ -7,25 +7,38 @@ public class Minesweeper {
 	private int sizeX;
 	private int sizeY;
 	private int nbMines;
-	
+
 	public static enum Difficulty {
 		EASY(10, 9, 9), MEDIUM(40, 16, 16), HARD(99, 30, 16);
-		
+
 		private int nbMines;
 		private int sizeX;
 		private int sizeY;
-		
-		private Difficulty(int nbMines, int sizeX, int sizeY){
+
+		private Difficulty(int nbMines, int sizeX, int sizeY) {
 			this.nbMines = nbMines;
 			this.sizeX = sizeX;
 			this.sizeY = sizeY;
 		}
+
+		public int getNbMines() {
+			return this.nbMines;
+		}
+
+		public int getSizeX() {
+			return this.sizeX;
+		}
+
+		public int getSizeY() {
+			return this.sizeY;
+		}
 	}
-	
+
 	public Minesweeper(Difficulty difficulty) {
+		this(difficulty.getNbMines(), difficulty.getSizeX(), difficulty.getSizeY());
 	}
-	
-	public Minesweeper(int nbMines, int sizeX, int sizeY){
-		
+
+	public Minesweeper(int nbMines, int sizeX, int sizeY) {
+
 	}
 }
