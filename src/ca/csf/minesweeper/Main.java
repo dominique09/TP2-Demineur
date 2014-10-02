b1b37c0b631b9c7275dc0301dac373d9ff8f3eb6
dominique09 <dominique.septembre@gmail.com>
2014-10-02 10:24:04 -0400

    FXML menu interface

--- src/ca/csf/minesweeper/Main.java
+++ src/ca/csf/minesweeper/Main.java
@@ -1,17 +1,17 @@
 package ca.csf.minesweeper;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
