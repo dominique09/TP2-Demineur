b4eecbed35788b7098317889187f92138fdc576d
Dominionized <dominique.begin.0@gmail.com>
2014-10-02 10:21:42 -0400

    Added Difficulty enum in Minesweeper class

--- src/ca/csf/minesweeper/Minesweeper.java
+++ src/ca/csf/minesweeper/Minesweeper.java
@@ -1,5 +1,31 @@
 package ca.csf.minesweeper;
 
-public class Minesweeper {
+import java.util.Hashtable;
 
+public class Minesweeper {
+	private Cell[] cellArray;
+	private int sizeX;
+	private int sizeY;
+	private int nbMines;
+	
+	public static enum Difficulty {
+		EASY(10, 9, 9), MEDIUM(40, 16, 16), HARD(99, 30, 16);
+		
+		private int nbMines;
+		private int sizeX;
+		private int sizeY;
+		
+		private Difficulty(int nbMines, int sizeX, int sizeY){
+			this.nbMines = nbMines;
+			this.sizeX = sizeX;
+			this.sizeY = sizeY;
+		}
+	}
+	
+	public Minesweeper(Difficulty difficulty) {
+	}
+	
+	public Minesweeper(int nbMines, int sizeX, int sizeY){
+		
+	}
 }
