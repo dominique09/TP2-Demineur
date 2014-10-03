998062fc3b4c1596e3422ca482c5eca65367cc31
dominique09 <dominique.septembre@gmail.com>
2014-10-03 09:52:31 -0400

    UI developpement

---
+++ src/ca/csf/minesweeper/HelpController.java
@@ -0,0 +1,22 @@
+package ca.csf.minesweeper;
+
+import javafx.fxml.FXML;
+import javafx.scene.web.WebView;
+import ca.csf.simpleFx.SimpleFXController;
+
+public class HelpController extends SimpleFXController{
+
+	@FXML
+	private WebView helpWebView;
+	
+	@FXML
+	public void initialize(){
+		helpWebView.getEngine().load(HelpController.class.getResource("help.html").toExternalForm());
+	}
+	
+	@FXML
+	public void closeHelp(){
+		this.getSimpleFxStage().close();
+	}
+	
+}
--- src/ca/csf/minesweeper/MainController.java
+++ src/ca/csf/minesweeper/MainController.java
@@ -1,5 +1,50 @@
 package ca.csf.minesweeper;
 
-public class MainController {
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import javafx.stage.StageStyle;
+import ca.csf.simpleFx.SimpleFXController;
+import ca.csf.simpleFx.SimpleFXScene;
+import ca.csf.simpleFx.SimpleFXStage;
+
+public class MainController extends SimpleFXController{
 	
+	
+	@FXML
+	private VBox gameContainer;
+	
+	@FXML
+	public void newGame(){
+		System.out.println("test");
+	}
+	
+	@FXML
+	public void openHelpWindow(){
+		try {
+			SimpleFXScene helpScene = new SimpleFXScene(HelpController.class.getResource("Help.fxml"), new HelpController());
+			
+			SimpleFXStage helpStage = new SimpleFXStage("Aide", StageStyle.UTILITY, helpScene, this.getSimpleFXApplication(), this.getSimpleFxStage());
+			
+			helpStage.setResizable(false);
+			helpStage.show();
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	@FXML
+	public void openInformations(){
+		try {
+			SimpleFXScene informationsScene = new SimpleFXScene(HelpController.class.getResource("Help.fxml"), new HelpController());
+			
+			SimpleFXStage informationsStage = new SimpleFXStage("Aide", StageStyle.UTILITY, informationsScene, this.getSimpleFXApplication(), this.getSimpleFxStage());
+			
+			informationsStage.setResizable(false);
+			informationsStage.show();
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 }
