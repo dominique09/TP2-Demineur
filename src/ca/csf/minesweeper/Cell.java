1ee18486a813d3117d6746957394a1af0a42b114
Dominionized <dominique.begin.0@gmail.com>
2014-10-06 12:40:21 -0400

    Changed flag and 'not sure' status in Cell class so they are independent of cell type.

--- src/ca/csf/minesweeper/Cell.java
+++ src/ca/csf/minesweeper/Cell.java
@@ -2,15 +2,19 @@ package ca.csf.minesweeper;
 
 public class Cell {
 	public static enum CellType {
-		EMPTY, MINE, FLAG, NOTSURE, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT
+		EMPTY, MINE, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT
 	}
 	
 	public boolean isHidden;
+	public boolean isFlagged;
+	public boolean isNotSure;
 	
 	public CellType type;
 	
 	public Cell(CellType type, boolean isHidden) {
 		this.type = type;
 		this.isHidden = isHidden;
+		this.isFlagged = false;
+		this.isNotSure = false;
 	}
 }
