9cc23d4a04e6ec2e621b3963a28c359c4a54cbe9
Dominique Bégin <dominique.begin.02gmail.com>
2014-11-03 13:09:06 -0500

    Changed isGameWon and scoreIsHighScore not to pass a boolean (it was useless);

--- src/ca/csf/minesweeper/MinesweeperObserver.java
+++ src/ca/csf/minesweeper/MinesweeperObserver.java
@@ -2,8 +2,8 @@ package ca.csf.minesweeper;
 
 public interface MinesweeperObserver {
 	public void playerIsDead();
-	public void gameIsWon(boolean gameIsWon);
-	public void scoreIsHighScore(boolean scoreIsHighScore);
+	public void gameIsWon();
+	public void scoreIsHighScore();
 	public void updateCell(int coordX, int coordY, Cell cell);
 	public void setNumberOfFlagsLeft(int nbFlags);
 }
