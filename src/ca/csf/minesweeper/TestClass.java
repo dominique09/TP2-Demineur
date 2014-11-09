59527d6a30dfee8b170ed17cd72db941867eb845
dominionized <dominique.begin.0@gmail.com>
2014-11-08 21:53:15 -0500

    Added various things to score files manipulation

--- src/ca/csf/minesweeper/TestClass.java
+++ src/ca/csf/minesweeper/TestClass.java
@@ -6,25 +6,27 @@ import java.io.InputStreamReader;
 public class TestClass {
 
 	public static void main(String[] args) {
-		Minesweeper msGame = new Minesweeper();
+		//Minesweeper msGame = new Minesweeper();
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			msGame.newGame(Minesweeper.Difficulty.MEDIUM);
+			//msGame.newGame(Minesweeper.Difficulty.MEDIUM);
 			
-			Scoreboard sb = new Scoreboard("scores.txt");
+			
+			Scoreboard sb = new Scoreboard("ecpizscoresz.txt");
 			
 			while(!sb.getScoreStack().isEmpty()){
 				Score score = sb.getScoreStack().pop();
 				System.out.println(score.name + "\t" + score.time + "\n");
 			}
-			sb.addScore(10, "WorldChampion");
+			sb.addScore(10, "WorldChampionBro");
+			sb.addScore(5, "Dominique");
 			
 			while (true){
 			System.out.println("Entrez un X");
 			int x = Integer.parseInt(br.readLine());
 			System.out.println("Entrez un Y");
 			int y = Integer.parseInt(br.readLine());
-			msGame.activate(x, y);
+			//msGame.activate(x, y);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
