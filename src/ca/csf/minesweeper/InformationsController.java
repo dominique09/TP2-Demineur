d67f865caa2ce77040c53ceefdf6f68c7a909868
dominique09 <dominique.septembre@gmail.com>
2014-11-10 13:33:50 -0500

    Added logo in informations

--- src/ca/csf/minesweeper/InformationsController.java
+++ src/ca/csf/minesweeper/InformationsController.java
@@ -2,10 +2,19 @@ package ca.csf.minesweeper;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import ca.csf.simpleFx.SimpleFXController;
 
 public class InformationsController extends SimpleFXController {
-
+	@FXML
+	public ImageView logoImageView;
+	
+	@FXML
+	public void initialize(){
+		logoImageView.setImage(new Image(this.getClass().getResource("resources/logo.png").toExternalForm()));;
+	}
+	
 	@FXML
 	public void closeInformations(ActionEvent event){
 		this.getSimpleFxStage().close();
